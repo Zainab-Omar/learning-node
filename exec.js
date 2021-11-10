10918559f23ae72ac4bd47a9a77a44056e98121e
Zainab Omar <zainab.alshaikhli01@gmail.com>
2021-11-10 17:36:52 -0600

    practice child process

---
+++ exec.js
@@ -0,0 +1,16 @@
+const cp = require("child_process")
+
+// cp.exec("open https://www.google.com/")
+
+// cp.exec("open -a Terminal .")
+
+// cp.exec("ls", (err, data, stderr) => {
+//     if (err){
+//         throw err
+//     }
+//     console.log(data)
+// })
+
+ cp.exec("node readStream", (err, data, stderr) => {
+    console.log(data)
+})
